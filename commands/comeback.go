fbcac4b63240dca6b5334035d1ee8d6ac093cbd0
Masaya Shinohara <shinohara@i-n.live>
2025-06-04 23:21:37 +0900

    add: comeback

--- commands/comeback.go
+++ commands/comeback.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/pyama86/slack-afk/go/presentation/blocks"
+	"github.com/pyama86/slack-afk/go/spreadsheet"
 	"github.com/pyama86/slack-afk/go/store"
 	"github.com/slack-go/slack"
 )
@@ -101,6 +102,14 @@ func (c *ComebackCommand) Execute(cmd slack.SlashCommand) error {
 		slog.Error("Failed to post ephemeral message", slog.Any("error", err))
 		return err
 	}
+
+	// 勤怠記録（エラーはログのみ）
+	go func() {
+		err := spreadsheet.AppendAttendanceRecord(c.client, uid, spreadsheet.TypeComeback, "")
+		if err != nil {
+			slog.Error("スプレッドシート勤怠記録失敗", slog.Any("error", err))
+		}
+	}()
 
 	return nil
 }
