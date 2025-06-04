9a05a521070a5dd25b1430e35445d1562315625a
Masaya Shinohara <shinohara@i-n.live>
2025-06-04 20:34:00 +0900

    Revert "update 実働時間などを実装"
    This reverts commit 42392bc7b27ec37a9a8809c32f391988b8275a90.

--- commands/comeback.go
+++ commands/comeback.go
@@ -5,17 +5,10 @@ import (
 	"log/slog"
 	"os"
 	"strings"
-	"context"
-	"io/ioutil"
-	"strconv"
-	"time"
 
 	"github.com/pyama86/slack-afk/go/presentation/blocks"
 	"github.com/pyama86/slack-afk/go/store"
 	"github.com/slack-go/slack"
-	"google.golang.org/api/option"
-	"google.golang.org/api/sheets/v4"
-	"golang.org/x/oauth2/google"
 )
 
 // ComebackCommand handles the /comeback command
@@ -109,125 +102,5 @@ func (c *ComebackCommand) Execute(cmd slack.SlashCommand) error {
 		return err
 	}
 
-	// 休憩終了時刻
-	jst, _ := time.LoadLocation("Asia/Tokyo")
-	now := time.Now().In(jst)
-
-	// Redisから休憩開始時刻を取得
-	lunchStartStr, err := c.redisClient.Get(uid+":lunch_start")
-	if err == nil && lunchStartStr != "" {
-		lunchStart, err := time.Parse(time.RFC3339, lunchStartStr)
-		if err == nil {
-			delta := int(now.Sub(lunchStart).Minutes())
-			// スプレッドシートに休憩時間加算・備考追記・実働時間再計算
-			spreadsheetID := os.Getenv("SPREADSHEET_ID")
-			if spreadsheetID != "" {
-				go func() {
-					err := updateLunchEndToSheet(spreadsheetID, userName, now.Format("2006-01-02"), delta, now.Format("15:04:05"))
-					if err != nil {
-						slog.Error("Failed to update lunch end to sheet", slog.Any("error", err))
-					}
-				}()
-			}
-		}
-	}
-
-	return nil
-}
-
-// スプレッドシートの本日行の休憩時間加算・備考追記・実働時間再計算
-func updateLunchEndToSheet(spreadsheetID, sheetName, dateStr string, addMinutes int, endTimeStr string) error {
-	ctx := context.Background()
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		return err
-	}
-	config, err := google.JWTConfigFromJSON(b, sheets.SpreadsheetsScope)
-	if err != nil {
-		return err
-	}
-	srv, err := sheets.NewService(ctx, option.WithHTTPClient(config.Client(ctx)))
-	if err != nil {
-		return err
-	}
-	readRange := fmt.Sprintf("%s!A:A", sheetName)
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
-	if err != nil {
-		return err
-	}
-	rowIndex := -1
-	for i, row := range resp.Values {
-		if len(row) > 0 && row[0] == dateStr {
-			rowIndex = i + 1
-			break
-		}
-	}
-	if rowIndex < 0 {
-		return fmt.Errorf("本日行が見つかりません")
-	}
-	// 休憩時間加算
-	dRange := fmt.Sprintf("%s!D%d", sheetName, rowIndex)
-	dResp, err := srv.Spreadsheets.Values.Get(spreadsheetID, dRange).Do()
-	if err != nil {
-		return err
-	}
-	dOld := 0
-	if len(dResp.Values) > 0 && len(dResp.Values[0]) > 0 {
-		dOld, _ = strconv.Atoi(fmt.Sprintf("%v", dResp.Values[0][0]))
-	}
-	dNew := dOld + addMinutes
-	_, err = srv.Spreadsheets.Values.Update(spreadsheetID, dRange, &sheets.ValueRange{Values: [][]interface{}{{dNew}}}).ValueInputOption("USER_ENTERED").Do()
-	if err != nil {
-		return err
-	}
-	// 備考追記
-	eRange := fmt.Sprintf("%s!E%d", sheetName, rowIndex)
-	eResp, err := srv.Spreadsheets.Values.Get(spreadsheetID, eRange).Do()
-	if err != nil {
-		return err
-	}
-	eOld := ""
-	if len(eResp.Values) > 0 && len(eResp.Values[0]) > 0 {
-		eOld = fmt.Sprintf("%v", eResp.Values[0][0])
-	}
-	eNew := eOld
-	if eOld != "" {
-		eNew += ", "
-	}
-	eNew += fmt.Sprintf("休憩終了:%s", endTimeStr)
-	_, err = srv.Spreadsheets.Values.Update(spreadsheetID, eRange, &sheets.ValueRange{Values: [][]interface{}{{eNew}}}).ValueInputOption("USER_ENTERED").Do()
-	if err != nil {
-		return err
-	}
-	// 実働時間再計算
-	// B:出勤, C:退勤, D:休憩(分)
-	bRange := fmt.Sprintf("%s!B%d", sheetName, rowIndex)
-	cRange := fmt.Sprintf("%s!C%d", sheetName, rowIndex)
-	bResp, err := srv.Spreadsheets.Values.Get(spreadsheetID, bRange).Do()
-	cResp, err2 := srv.Spreadsheets.Values.Get(spreadsheetID, cRange).Do()
-	if err != nil || err2 != nil {
-		return nil // 出勤・退勤がなければスキップ
-	}
-	bStr, cStr := "", ""
-	if len(bResp.Values) > 0 && len(bResp.Values[0]) > 0 {
-		bStr = fmt.Sprintf("%v", bResp.Values[0][0])
-	}
-	if len(cResp.Values) > 0 && len(cResp.Values[0]) > 0 {
-		cStr = fmt.Sprintf("%v", cResp.Values[0][0])
-	}
-	if bStr != "" && cStr != "" {
-		bTime, err1 := time.Parse("15:04:05", bStr)
-		cTime, err2 := time.Parse("15:04:05", cStr)
-		if err1 == nil && err2 == nil {
-			workMin := int(cTime.Sub(bTime).Minutes()) - dNew
-			if workMin < 0 {
-				workMin = 0
-			}
-			hh := workMin / 60
-			mm := workMin % 60
-			fRange := fmt.Sprintf("%s!F%d", sheetName, rowIndex)
-			_, _ = srv.Spreadsheets.Values.Update(spreadsheetID, fRange, &sheets.ValueRange{Values: [][]interface{}{{fmt.Sprintf("%02d:%02d", hh, mm)}}}).ValueInputOption("USER_ENTERED").Do()
-		}
-	}
 	return nil
 }
